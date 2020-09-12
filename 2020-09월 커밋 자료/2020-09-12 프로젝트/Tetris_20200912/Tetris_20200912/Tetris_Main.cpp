e81be5ff25999304d2078c8d0f101af24d21b23d
leesangjn <kruore1321@gmail.com>
2020-09-12 23:59:58 +0900

    CurrentMasterCheck
    11

---
+++ 2020-09월 커밋 자료/2020-09-12 프로젝트/Tetris_20200912/Tetris_20200912/Tetris_Main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	int a;
+	while (1) {
+		printf("숫자입력(1:초기화, 2: 종료) : ");
+		scanf_s("%d", &a);
+		if (a == 1) {
+			system("cls");
+		}
+		else if (a == 2) {
+			break;
+		}
+	}
+}
